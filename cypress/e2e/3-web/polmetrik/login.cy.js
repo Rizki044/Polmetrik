7e97d00dae4630c9d2089a89f8c22701956ab202
umardanimarcelo <muh.umardani1988@gmail.com>
2024-02-07 17:38:53 +0700

    add

--- cypress/e2e/3-web/polmetrik/login.cy.js
+++ cypress/e2e/3-web/polmetrik/login.cy.js
@@ -1,33 +1,33 @@
 describe('Login Functionality', () => {
   beforeEach(() => {
-      cy.visit('https://app.polmetrik.id/login')
+      cy.visit('https://app.polmetrik.id/')
 })
 
   it('Verify Failed Login invalid email address', () => {
-      cy.get('#email').type('')
-      cy.get('#pass').type('')
+      cy.get('[:rc:-label]').type('wakidah@')
+      cy.get('#Kata.Sandi').type('Tina2024')
       cy.get('.action.login.primary').click()
 
       cy.get('#email-error').should('be.visible')
   })
 
 it('Verify Failed Login Without email address', () => {
-    cy.get('#pass').type('')
+    cy.get('#pass').type('Tina2024')
     cy.get('.action.login.primary').click()
 
     cy.get('.message-error').should('be.visible')
 })
 
 it('Verify Failed Login Without password', () => {
-    cy.get('#email').type('')
+    cy.get('#email').type('wakidah@binokular.net')
     cy.get('.action.login.primary').click()
 
     cy.get('.message-error').should('be.visible')
 })
 
 it('Verify Failed Login Wrong Password', () => {
-    cy.get('#email').type('')
-    cy.get('#pass').type('')
+    cy.get('#email').type('wakidah@binokular.net')
+    cy.get('#pass').type('tina2024')
     cy.get('.action.login.primary').click()
 
     cy.get('.message-error').should('be.visible')
