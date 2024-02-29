b11e7ca57c914cd658ad3f37ff39cbdeeb790596
umardanimarcelo <muh.umardani1988@gmail.com>
2024-02-29 17:32:20 +0700

    update

--- cypress/e2e/3-web/polmetrik/downloadnkonsultan.cy.js
+++ cypress/e2e/3-web/polmetrik/downloadnkonsultan.cy.js
@@ -9,9 +9,13 @@ it("Download dan Konsultan", () => {
     cy.get('[href="/download-contact"] > .MuiList-root > .MuiButtonBase-root > .MuiListItemText-root > .MuiTypography-root').click();
     //Download Raw Data
     cy.get(':nth-child(1) > a > .MuiList-root > .MuiListItem-root > .MuiListItemText-root > .MuiTypography-root > h1').click();
-    cy.get(':nth-child(2) > .MuiButton-outlined').click;
-    cy.get('.MuiButtonBase-root > [data-testid="ArrowDropDownIcon"]').click();
+    cy.wait(500)
+    cy.get(':nth-child(2) > .MuiButton-outlined').click()
+    cy.wait(500)
+    cy.get('.MuiButtonBase-root > [data-testid="ArrowDropDownIcon"]').type("Online")
+    cy.wait(500)
     
+
 
 
 
